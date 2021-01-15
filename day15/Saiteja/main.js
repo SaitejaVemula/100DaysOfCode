49f2f7d4f460d757fe4c0ee89d61b36f8a455229
SaitejaVemula <saitejavemula@gmail.com>
2021-01-15 23:15:11 +0530

    Solved deleteFormArray()

--- day15/Saiteja/main.js
+++ day15/Saiteja/main.js
@@ -27,6 +27,33 @@ function insertIntoArray(arr, insertNumber, capacity, position) {
     return arr;
 }
 
-function deleteFromArray() {
+function deleteFromArray(arr, deleteNumber) {
+    if(arr.includes(deleteNumber)) {
+        let tempArr = [];
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i] !== deleteNumber) {
+                tempArr.push(arr[i]);
+            }
+        }
+        // console.log(tempArr, arr);
+        arr = tempArr;
+    } else {
+        console.log(`${deleteNumber} doesn't exists in the array`);
+    }
 
+    return arr;
 }
+console.log(`
+You're given an array of size N, and number X, capacity C, and position P... you have to create functions for insertion
+and delete on array.
+
+insertIntoArray(N, X, C, P) and deleteFromArray(N, X)
+
+Note: Array size is fixed, if capacity is full you can not insert into it. and for delete function you can't use
+position.
+
+Note: Don't use inbuilt methods
+
+eg: input: arr -> [10,20,30,40,50,60] X->70 Capactiy-->7 and position is 3
+output: [10,20,70,30,40,50,60]
+`);
